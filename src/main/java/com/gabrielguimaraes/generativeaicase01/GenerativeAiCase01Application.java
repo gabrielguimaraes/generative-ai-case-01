bd71958536c9ada329b3d8017292ea4e05c1b115
Gabriel Guimaraes <gabguim@gmail.com>
2023-09-07 19:05:07 +0200

    adding spring boot project with changes on pom.xml for sonarcloud.io

---
+++ src/main/java/com/gabrielguimaraes/generativeaicase01/GenerativeAiCase01Application.java
@@ -0,0 +1,13 @@
+package com.gabrielguimaraes.generativeaicase01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerativeAiCase01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerativeAiCase01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielguimaraes/generativeaicase01/GenerativeAiCase01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielguimaraes.generativeaicase01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerativeAiCase01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
